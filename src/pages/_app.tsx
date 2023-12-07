1fc5066b1db97bfc0b170348c2eb3e5454145019
Thomás Dixini <thomasdixini@gmail.com>
2023-12-07 01:05:55 -0300

    :sparkles: feat: Página principal do projeto criada

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,17 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { Anton } from "next/font/google";
+
+const anton = Anton({
+  weight: "400",
+  subsets: ["latin"],
+  display: "swap",
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={anton.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,45 @@
-import { Roboto } from 'next/font/google'
- 
-const roboto = Roboto({
-  weight: '900',
-  subsets: ['latin'],
-  display: 'swap',
-})
+import styles from "@/styles/home/Home.module.css";
+import { FaLinkedin, FaGithubSquare } from "react-icons/fa";
+import { GrNotes } from "react-icons/gr";
 
 export default function Home() {
   return (
-    <div className={roboto.className}>
-      <h1> Hello World </h1>
-    </div>
+    <main className={styles.main}>
+      <section className={styles.userSection}>
+        <span> Oi✌️, Eu sou Thomás Dixini </span>
+        <div className={styles.description}>
+          <div className={styles.separationLinks}>
+            <span> REACT </span>
+            <ul className={styles.ul}>
+              <li className={styles.li}>
+                <a href="" className={styles.a}>
+                  {" "}
+                  <FaLinkedin />{" "}
+                </a>
+              </li>
+              <li className={styles.li}>
+                <a href="" className={styles.a}>
+                  {" "}
+                  <FaGithubSquare />{" "}
+                </a>
+              </li>
+              <li className={styles.li}>
+                <a href="" className={styles.a}>
+                  {" "}
+                  <GrNotes />{" "}
+                </a>
+              </li>
+            </ul>
+          </div>
+          <span>DEVELOPER</span>
+        </div>
+        <p className={styles.p}>
+          TENHO 21 ANOS, SOU FORMADO EM CIÊNCIAS DA COMPUTAÇÃO E ATUALMENTE{" "}
+          <br />
+          ESTOU ADQUIRINDO EXPERIÊNCIA COM DESENVOLVIMENTO WEB ATRAVÉS DE <br />
+          PROJETOS PESSOAIS
+        </p>
+      </section>
+    </main>
   );
 }
